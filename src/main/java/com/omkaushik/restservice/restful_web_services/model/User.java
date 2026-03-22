29c56dd209244584a0b99e9b674f1d78867ce1b6
Om Kumar Kaushik <omkumar.kaushik@gmail>
2026-03-22 22:27:46 +0530

    added new entity with relationships

--- src/main/java/com/omkaushik/restservice/restful_web_services/model/User.java
+++ src/main/java/com/omkaushik/restservice/restful_web_services/model/User.java
@@ -1,10 +1,14 @@
 package com.omkaushik.restservice.restful_web_services.model;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -24,6 +28,10 @@ public class User {
 	
 	@Past(message = "Date of birth should always be in past")
 	private LocalDate dateOfBirth;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	public User(Integer id, String name, LocalDate dateOfBirth) {
 		super();
@@ -56,9 +64,21 @@ public class User {
 		this.dateOfBirth = dateOfBirth;
 	}
 
+	public User(Integer id, @Size(min = 2, message = "Username must be of at least two characters") String name,
+			@Past(message = "Date of birth should always be in past") LocalDate dateOfBirth, Post posts) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + "]";
+		return "User [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", posts=" + posts + "]";
+	}
+
+	public List<Post> getPosts() {
+		return posts;
 	}
 	
 }
